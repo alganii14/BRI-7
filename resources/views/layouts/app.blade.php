9448e962570357e99799e9e2e26b8ea9fdb1a2f1
alganii14 <alghani8388@gmail.com>
2025-11-30 16:40:09 +0700

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -939,12 +939,6 @@
                                 </svg>
                                 Merchant EDC Savol
                             </a>
-                            <a href="{{ route('manager-pull-pipeline.penurunan-merchant') }}" class="sub-submenu-item {{ request()->routeIs('manager-pull-pipeline.penurunan-merchant') ? 'active' : '' }}">
-                                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" style="width: 14px; height: 14px; margin-right: 8px;">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
-                                </svg>
-                                Penurunan Merchant
-                            </a>
                             <a href="{{ route('manager-pull-pipeline.penurunan-casa-brilink') }}" class="sub-submenu-item {{ request()->routeIs('manager-pull-pipeline.penurunan-casa-brilink') ? 'active' : '' }}">
                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" style="width: 14px; height: 14px; margin-right: 8px;">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
